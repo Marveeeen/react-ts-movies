5041a543e1342ae5d6a0ae8515cf1208619eebb4
Marveeen <jmvroqueee@gmail.com>
2023-02-18 21:21:38 +0800

    implement sample static component and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import MovieList from "./components/MovieList";
+import MovieList from "./components/Movies";
+import { TMovieList } from "./components/Movies/types";
+import { generateMockMovieList } from "./components/utils/utils";
 
 import { Section, Button } from "./styles";
 
 const App = () => {
+  const [movieList, setMovieList] = useState<TMovieList>([]);
+
+  useEffect(() => {
+    setMovieList(generateMockMovieList(20));
+  }, []);
+
+  const fetchHandler = () => {
+    setMovieList(generateMockMovieList(Math.floor((Math.random() * 20) + 1)));
+  };
+
   return (
     <React.Fragment>
       <Section>
-        <Button>Fetch movies</Button>
+        <Button onClick={fetchHandler}>Fetch movies</Button>
+      </Section>
+      <Section>
+        <MovieList movieList={movieList} />
       </Section>
     </React.Fragment>
   );
